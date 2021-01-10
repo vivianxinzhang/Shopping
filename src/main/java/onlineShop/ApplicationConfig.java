ced44098a8e149c821d308377e261627f8b6c3bb
Vivian Zhang <vivianxinzhang@gmail.com>
2021-01-10 11:56:16 -0800

    MVC implementation of homepage and register

--- src/main/java/onlineShop/ApplicationConfig.java
+++ src/main/java/onlineShop/ApplicationConfig.java
@@ -2,13 +2,29 @@ package onlineShop;
 
 import java.util.Properties;
 import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
+@PropertySource("classpath:configure.properties")
+@EnableWebMvc
 public class ApplicationConfig {
+    @Value( "${USERNAME}" )
+    private String username;
+
+    @Value( "${PASSWORD}" )
+    private String password;
+
+//    @Autowired
+//    private Environment env;
 
     @Bean(name = "sessionFactory")
     public LocalSessionFactoryBean sessionFactory() {
@@ -24,12 +40,17 @@ public class ApplicationConfig {
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        // 只需要修改红色部分, 保留其他内容
 
 //        dataSource.setUrl("jdbc:mysql://YOUR_RDS_IP:3306/ecommerce?createDatabaseIfNotExist=true&serverTimezone=UTC");
+//        dataSource.setUsername("USERNAME");
+//        dataSource.setPassword("PASSWORD");
+
         dataSource.setUrl("jdbc:mysql://mysqldatabase.cw8kc8mzwiml.us-east-2.rds.amazonaws.com:3306/ecommerce?createDatabaseIfNotExist=true&serverTimezone=UTC");
-        dataSource.setUsername("USERNAME");
-        dataSource.setPassword("PASSWORD");
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+//        dataSource.setUsername(env.getProperty("USERNAME"));
+//        dataSource.setPassword(env.getProperty("PASSWORD"));
 
         return dataSource;
     }
@@ -38,7 +59,8 @@ public class ApplicationConfig {
     private final Properties hibernateProperties() {
         Properties hibernateProperties = new Properties();
         hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
-        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect"); // if mysql version is 8, MySQL5InnoDBDialect
+        // hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+        // if mysql version is 8, MySQL5InnoDBDialect
         hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5InnoDBDialect");
         hibernateProperties.setProperty("hibernate.show_sql", "true");
         hibernateProperties.setProperty("hibernate.format_sql", "true");
